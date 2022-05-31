d14fd91644177aa40b4c457065d7febf02cc0aad
Daniel Aguilera <daguilera@sklum.com>
2022-05-31 12:02:47 +0200

    feat(MS-328): remove repo name input

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,14 @@ import { getInput, setFailed } from '@actions/core';
 
 async function repositoryDispatch() {
   const token: string = getInput('token');
-  const owner: string = getInput('repo').split('/')[0];
-  const repo: string = getInput('repo').split('/')[1];
   const branch: string = getInput('branch');
   const octokit = new Octokit({
     auth: token
   });
 
   await octokit.repos.createDispatchEvent({
-    owner,
-    repo,
+    owner: 'ndcmsl',
+    repo: 'ms-deploy',
     event_type: 'deploy-api-doc',
     client_payload: {
       branch
